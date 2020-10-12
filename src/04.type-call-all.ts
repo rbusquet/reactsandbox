ccd6ed8a84674fa68b612e6c99f937b9e5f0028b
Ricardo Busquet <ricardobusquet@gmail.com>
2020-10-12 11:47:55 -0400

    Fix callAll type

--- src/04.type-call-all.ts
+++ src/04.type-call-all.ts
@@ -1,13 +1,15 @@
-const callAll = <T extends (...args: any) => any>(...fns: T[]) => (
-  ...args: Parameters<T>
-) => fns.forEach(fn => fn?.(...args));
+function callAll<T extends (...args: any[]) => any>(...fns: T[]) {
+  return (...args: Parameters<T>) => fns.forEach(fn => fn?.(...args));
+}
 
 const fns = [
   (x: number) => x,
   (s: string) => s,
   (a: string, b: number) => console.log({ a, b }),
+  () => console.log("oops"),
 ];
 
-callAll(...fns)(1);
+const callAllResult = callAll(...fns);
+callAllResult("1", 2);
 
 export { callAll };
