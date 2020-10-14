fdfce4049080059a6d0932c48603b4851df2dcff
Ricardo Busquet <ricardobusquet@gmail.com>
2020-10-14 09:47:52 -0400

    Add destructoring example

---
+++ src/06-destructoring.ts
@@ -0,0 +1,31 @@
+function nestedArrayAndObject() {
+  // refactor this to a single line of destructuring...
+  const info = {
+    title: "Once Upon a Time",
+    protagonist: {
+      name: "Emma Swan",
+      enemies: [
+        { name: "Regina Mills", title: "Evil Queen" },
+        { name: "Cora Mills", title: "Queen of Hearts" },
+        { name: "Peter Pan", title: `The boy who wouldn't grow up` },
+        { name: "Zelena", title: "The Wicked Witch" },
+      ],
+    },
+  };
+  // const {} = info // <-- replace the next few `const` lines with this
+  const {
+    title,
+    protagonist: {
+      name: protagonistName,
+      enemies: [, , , { title: enemyTitle, name: enemyName }],
+    },
+  } = info;
+  //   const title = info.title
+  //   const protagonistName = info.protagonist.name
+  //   const enemy = info.protagonist.enemies[3]
+  //   const enemyTitle = enemy.title
+  //   const enemyName = enemy.name
+  return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`;
+}
+
+export { nestedArrayAndObject };
