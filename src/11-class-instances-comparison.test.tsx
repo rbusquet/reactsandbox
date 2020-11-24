7e0ee182a07c4958be45c780cf3cb5b581ace293
Ricardo Busquet <ricardobusquet@gmail.com>
2020-11-24 12:33:20 -0500

    toStrictEqual fails when type is different

--- src/11-class-instances-comparison.test.tsx
+++ src/11-class-instances-comparison.test.tsx
@@ -1,7 +1,28 @@
-import { Item, Cheese } from "./11-class-instances-comparison";
+export class Item {
+  name: string;
+  daysLeft: number;
+  quality: number;
+  constructor(name: string, daysLeft: number, quality: number) {
+    this.name = name;
+    this.daysLeft = daysLeft;
+    this.quality = quality;
+  }
+}
+
+export class Cheese extends Item {
+  constructor(daysLeft: number, quality: number) {
+    super("Cheese", daysLeft, quality);
+  }
+}
 
 test("Cheese == Item???", () => {
   const cheeseItem = new Item("Cheese", 10, 10);
   const cheese = new Cheese(10, 10);
   expect(cheeseItem).toEqual(cheese);
+});
+
+test("Cheese != Item!", () => {
+  const cheeseItem = new Item("Cheese", 10, 10);
+  const cheese = new Cheese(10, 10);
+  expect(() => expect(cheeseItem).toStrictEqual(cheese)).toThrow();
 });
